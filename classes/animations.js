bd16c0c7d6e36818d26e13273be54d99bef222db
Peter-Paul <pp@ccode.nl>
2020-09-17 18:31:52 +0200

    1

--- classes/animations.js
+++ classes/animations.js
@@ -1,39 +1,35 @@
 class Animations{
-    
-    static ease1;
-    static ease2;
-    static animationCam;
-    static animationTarget;
+
     
     static CameraTargetToPosition(cam, node, frameCount, onFinish)
     {
         scene.stopAnimation(cam);
         var newPos = node.position;
-        this.ease1 = new BABYLON.SineEase();
-        this.ease1.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
-        this.animationTarget = BABYLON.Animation.CreateAndStartAnimation('at5', cam, 'target', 20, frameCount, cam.target, newPos, 0, this.ease1, onFinish);
-        this.animationTarget.disposeOnEnd = true;
+        var ease1 = new BABYLON.SineEase();
+        ease1.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+        var animationTarget = BABYLON.Animation.CreateAndStartAnimation('at5', cam, 'target', 20, frameCount, cam.target, newPos, 0, this.ease1, onFinish);
+        animationTarget.disposeOnEnd = true;
     }
 
     static CameraToPosition(cam, newPos, frameCount, onFinish) {
-        this.ease2 = new BABYLON.SineEase();
-        this.ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEIN);
-        this.animationCam = BABYLON.Animation.CreateAndStartAnimation('at4', cam, 'position', 20, frameCount, cam.position, newPos, 0, this.ease2, onFinish);
-        this.animationCam.disposeOnEnd = true;
+        var ease2 = new BABYLON.SineEase();
+        ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEIN);
+        var animationCam = BABYLON.Animation.CreateAndStartAnimation('at4', cam, 'position', 20, frameCount, cam.position, newPos, 0, this.ease2, onFinish);
+        animationCam.disposeOnEnd = true;
     }
 
     static CameraToRadius(cam, radius, frameCount, onFinish) {
-        this.ease2 = new BABYLON.SineEase();
-        this.ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
-        this.animationCam = BABYLON.Animation.CreateAndStartAnimation('at1', cam, 'radius', 20, frameCount, cam.radius, radius, 0, this.ease2, onFinish);
-        this.animationCam.disposeOnEnd = true;
+        var ease2 = new BABYLON.SineEase();
+        ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+        var animationCam = BABYLON.Animation.CreateAndStartAnimation('at1', cam, 'radius', 20, frameCount, cam.radius, radius, 0, this.ease2, onFinish);
+        animationCam.disposeOnEnd = true;
     }
 
     static CameraToStartPosition(cam, radius, frameCount, onFinish) {
-        this.ease2 = new BABYLON.SineEase();
-        this.ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
-        this.animationCam = BABYLON.Animation.CreateAndStartAnimation('at6', cam, 'radius', 20, frameCount, cam.radius, radius, 0, this.ease2, onFinish);
-        this.animationCam.disposeOnEnd = true;
+        var ease2 = new BABYLON.SineEase();
+        ease2.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+        var animationCam = BABYLON.Animation.CreateAndStartAnimation('at6', cam, 'radius', 20, frameCount, cam.radius, radius, 0, this.ease2, onFinish);
+        animationCam.disposeOnEnd = true;
 
         var anim = BABYLON.Animation.CreateAndStartAnimation('at7', cam, 'beta', 20, frameCount, cam.beta, scene.cameraBeta, 0, this.ease2, null);
         anim.disposeOnEnd = true;
